e987d9b38784dc1a4b12a22d104fd1a05a14ac31
Arif <115531149+whynotdev@users.noreply.github.com>
2024-03-12 23:37:13 +0530

    intial

---
+++ dbConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const dbURL = process.env.dbURL;
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.on('error', (err) => console.log(err));
+
+connection.on('connected', () =>
+  console.log('Mongo DB Connection Successful')
+);
